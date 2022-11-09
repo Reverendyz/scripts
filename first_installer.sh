d547af982ebdbdf5085b5120f1061e822a8e5126
Felipe moreira dos Santos <fmsantos1997@hotmail.com>
2022-11-09 13:14:39 -0300

    Added scripts

--- first_installer.sh
+++ first_installer.sh
@@ -11,3 +11,4 @@ fi
 curl -sLO "https://d1kpmuwb7gvu1i.cloudfront.net/ftkimager.3.1.1_ubuntu64.tar.gz" && \
     tar -zxvf ftkimager.3.1.1_ubuntu64.tar.gz && \
     install ftkimager /usr/local/bin/
+    
